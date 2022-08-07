07b2ae49b6967ea59496280d46fdc9810418be41
Shivam <shivammadlani5@gmail.com>
2022-08-07 19:39:14 +0530

    design changes

--- src/Result.js
+++ src/Result.js
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import './Results.css';
 
 const Result = ({ query }) => {
     const [error, setError] = useState(false);
@@ -6,11 +7,12 @@ const Result = ({ query }) => {
     const [items, setItems] = useState([]);
 
     useEffect(() => {
-        fetch(`https://kgsearch.googleapis.com/v1/entities:search?query=${query}&key=AIzaSyBtnD9Fk9ZUWJjg98O-UgXl8f9zepdMU0I&limit=10&indent=true`)
+        fetch(`https://kgsearch.googleapis.com/v1/entities:search?query=${query}&key=AIzaSyBtnD9Fk9ZUWJjg98O-UgXl8f9zepdMU0I&limit=5&indent=true`)
             .then(response => response.json())
             .then((data) => {
+                console.log(data.itemListElement);
                 setIsLoaded(true);
-                setItems(data);
+                setItems(data.itemListElement);
             }, (error) => {
                 setIsLoaded(true);
                 setError(true);
@@ -18,21 +20,21 @@ const Result = ({ query }) => {
         // eslint-disable-next-line
     }, [query]);
 
-
     if (error) {
         return <div className="error">Error!</div>
     } else if (!isLoaded) {
         return <div className="loading">Loading...</div>
     } else {
         return (
-            <div>Rendered
-                {/* <ul>
-                    {isLoaded && (items.map(item => (
-                        <li key={item.id}>
-                            {item.name}
+            <div className="searchContainer">
+                <ul className="searchItems">
+                    {isLoaded && (Object.values(items).map((item, ind) => (
+                        <li className="searchResults" key={ind}>
+                            <a href={item.result.url} target="_blank" rel="noopener noreferrer">{item.result.name}</a>
                         </li>
-                    )))}
-                </ul> */}
+                    )
+                    ))}
+                </ul>
             </div>
         );
     }
